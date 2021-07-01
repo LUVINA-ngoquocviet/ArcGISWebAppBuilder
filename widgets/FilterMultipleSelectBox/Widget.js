24acbc275ee2d8dc96061e033ed97802fbc59e85
NamLH <luuhoainam@luvina.net>
2021-07-01 17:23:07 +0700

    commit chức năng FilterMultipleSelectBox

--- widgets/FilterMultipleSelectBox/Widget.js
+++ widgets/FilterMultipleSelectBox/Widget.js
@@ -1,4 +1,4 @@
-var opLayer_KaisouBetsu = new Object();
+var opLayer_Prefectures = new Object();
 define([
   "dojo/_base/declare",
   "dojo/_base/lang",
@@ -16,6 +16,7 @@ define([
   "dojo/data/ObjectStore",
   "dojo/on",
   "widgets/common",
+  "dojo/json"
 ], function (
   declare,
   lang,
@@ -30,7 +31,8 @@ define([
   Memory,
   ObjectStore,
   on,
-  common
+  common,
+  json
 ) {
   //To create a widget, you need to derive from BaseWidget.
   return declare([BaseWidget], {
@@ -49,7 +51,6 @@ define([
       this.listenWidgetIds.push("framework");
 
       this.filterManager = FilterManager.getInstance();
-      // this.layerStructure = LayerStructure.getInstance();
     },
 
     startup: function () {
@@ -65,28 +66,20 @@ define([
       // common._initKengen();
       // common._eventHandlerPrefecture();
 
-      // Create select box
-      self._createSelect(self);
-
       this.opLayers = this.map.itemInfo.itemData.operationalLayers;
 
+      //Filter layer
       array.some(
         this.opLayers,
         lang.hitch(this, function (layer) {
           if (layer.layerType === "ArcGISFeatureLayer") {
             var title_array = layer.title.split(" ");
-            if (title_array.length != 3) {
-              return;
-            }
-            var layerName = title_array[2];
+            var layerName = title_array[0];
             switch (layerName) {
               case "japan_Prefectures":
                 self.prefecture_layer_id = layer.id;
-                opLayer_KaisouBetsu = new FeatureLayer(layer.layerObject.url);
-
-                self._filter_kaisou(self, common.area_shiten_cd);
-
-                self._initJigyoshoSelect(self);
+                opLayer_Prefectures = new FeatureLayer(layer.layerObject.url);
+                self._createSelectBox(self, opLayer_Prefectures);
                 break;
               default:
                 break;
@@ -96,123 +89,48 @@ define([
       );
     },
 
-    _initJigyoshoSelect: function (self) {
-      var o10 = self._createListData(
-        self,
-        "10",
-        "",
-        "shisha",
-        common.area_shisha_cd
-      );
-      self._sleep(1, self._initJigyoshoSelect10Fin(self, o10));
-    },
-
-
-    _initJigyoshoSelect10Fin: function (self, o10) {
-      if (!o10.isFulfilled()) {
-        self._sleep(1, function () {
-          self._initJigyoshoSelect10Fin(self, o10);
-        });
-      } else {
-        if (common.kengen_lv != "00") {
-          common._setDomVal("shisha", "disabled", true);
-          var o20 = self._createListData(
-            self,
-            "20",
-            common.area_shisha_cd,
-            "shukan",
-            common.area_shukan_center_id
-          );
-          self._initJigyoshoSelect20Fin(self, o20);
-        } else {
-          common._setDomVal("shukan", "disabled", true);
-          common._setDomVal("shiten", "disabled", true);
-          common._setDomVal("center", "disabled", true);
-          self._sleep(1, function () {
-            self.init_select = false;
-            self._loadingAnim(self);
-          });
-        }
-      }
-    },
-
-    /*30Fin, 40Fin...
-     */
-
-    _loadingAnim: function (self) {
-      if (common.isAppConfigLoaded) {
-        html.addClass(jimuConfig.loadingId, "loading-fadeOut");
-        this.own(
-          on(
-            document.getElementById(jimuConfig.loadingId),
-            "animationend",
-            lang.hitch(this, function () {
-              html.setStyle(jimuConfig.loadingId, "display", "none");
-            })
-          )
-        );
-      } else {
-        console.log("common.isAppConfigLoaded :" + common.isAppConfigLoaded);
-        console.log("obj.isFulfilled :" + obj.isFulfilled());
-        self._sleep(5, self._loadingAnim(self));
-      }
-    },
-
-    _sleep: function (waitSec, callbackFunc) {
-      var spanedSec = 0;
-      var id = setInterval(function () {
-        spanedSec++;
-        if (spanedSec >= waitSec) {
-          clearInterval(id);
-          if (callbackFunc) callbackFunc();
-        }
-      }, 100);
-    },
-
-    _createListData: function (self, kbn, jigyosho_cd, objId, value) {
-      var expr = "";
-
-      if (kbn == "10") {
-        expr = "kaisou_kbn = '10' and del_flg = '0'";
-      } else if (kbn == "20") {
-        expr =
-          "kaisou_kbn = '10' and shisha_cd = " +
-          jigyosho_cd +
-          "' and del_flg = '0'";
-      } //.... 30,40
-
+    //Create Select Box with layer
+    _createSelectBox: function (self, layerFeature) {
       var query = new Query();
       query.returnGeometry = false;
-      query.returnDistinctValues = true;
-      query.outFields = ["PRN"];
-      query.orderByFields = ["PRN"];
-      // query.where = expr;
+      query.outFields = ["*"];
+      query.where = "1 = 1";
 
-      var list = new Array(empty);
-      var obj = opLayer_KaisouBetsu
-        .queryFeatures(query)
-        .then(function (response) {
-          list = list.concat(
-            response.features.map(function (item) {
-              return {
-                label: item.attributes.PRN.trim(),
-                id: item.attributes.PRN.trim(),
-              };
-            })
+      //Get data from layer
+      var dataArr = [];
+      layerFeature.queryFeatures(query).then(
+        function (response) {
+          response.features.forEach(function (feature) {
+            var itemArr = { label: feature.attributes["PRN"], value: feature.attributes["FID"] };
+            dataArr.push(itemArr)
+          }
           );
-          var objectStore = new ObjectStore({
-            objectStore: new Memory({ data: list }),
-          });
-          dijit.byId(objId).set("store", objectStore);
-          dijit.byId(objId).set("value", value);
-        });
-      return obj;
+        },
+        function (e) {
+          console.log(e);
+        }
+      );
+
+      //Create Select Box
+      new Select({
+        id: "prefecture",
+        options: dataTest,
+        style: "width: 200px;",
+        sortByLabel: false,
+        onChange: function (state) {
+          //zoom to map with related data
+          self._filter_kaisou(self, state);
+        },
+      })
+        .placeAt(self.searchNodePrefecture)
+        .startup();
     },
 
+    //zoom to map with related data
     _filter_kaisou: function (self, state) {
       var expr = "";
-      expr += "jigyosho_cd ='" + state + "' and ";
-      expr += "del_flg = '0'";
+      expr += "FID ='" + state + "'";
+
       self.filterManager.applyWidgetFilter(
         self.prefecture_layer_id,
         self.id,
@@ -221,64 +139,13 @@ define([
 
       var layerStructure = LayerStructure.getInstance();
       var layer = layerStructure.getNodeById(self.prefecture_layer_id);
+
       var obj = layer.getExtent().then(
         lang.hitch(layer, function (extend) {
           layer.zoomTo(extend);
         })
       );
       return obj;
-    },
-
-    _filter_kuiki: function(self){
-      var expr = "";
-      if (common.area_center_cd != "-") {
-        expr += "center_ten_cd = '" + common.area_center_cd + "' and ";
-      }//....shiten_cd, shukan_cd, shisha_cd
-      expr += "del_flg = '0'";
-
-      // self.filterManager.applyWidgetFilter(self.syuhai_kuiki_layer_id, self.id, expr);
-    },
-
-
-    _createSelect: function (self) {
-      new Select({
-        id: "prefecture",
-        store: new ObjectStore({
-          objectStore: new Memory({ data: new Array(empty) }),
-        }),
-        style: "width: 200px;",
-        sortByLabel: false,
-        onChange: function (state) {
-          console.log("call shiten onchange :" + state);
-          if (!self.init_select) {
-            console.log("shiten onchange :" + state);
-            common.area_shiten_cd = state;
-            common.area_center_cd = empty.id;
-
-            if (state !== empty.id) {
-              // shisha sentaku toki
-              var objectStore = new ObjectStore({
-                objectStore: new Memory({ data: new Array(empty) }),
-              });
-              common._setDomVal("center", "disabled", false);
-              self._createListData(self, "40", state, "center", "-");
-
-              self._filter_kaisou(self, state);
-            } else {
-              // sentaku nashi
-              var objectStore = new ObjectStore({
-                objectStore: new Memory({ data: new Array(empty) }),
-              });
-              dijit.byId("center").set("store", objectStore);
-              common._setDomVal("center", "disabled", true);
-            }
-            self._filter_kuiki(self);
-            common._eventHandlerPrefecture();
-          }
-        },
-      })
-        .placeAt(self.searchNodePrefecture)
-        .startup();
     },
   });
 });
